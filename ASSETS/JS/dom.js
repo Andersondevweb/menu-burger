30f3d1eb7439870ee7c2fc59e3fe8b9757d8c14b
Andersondevweb <andersonn.alexandre1@gmail.com>
2022-08-29 10:23:51 +0200

    ajout

---
+++ ASSETS/JS/dom.js
@@ -0,0 +1,29 @@
+function h1(text) {
+    var div = document.createElement('div');
+    document.appendChild(div);
+    var h1 = document.createElement('h1');
+    div.appendChild(h1);
+    h1.createTextNode(text);
+    const Nom_Text = document.createTextNode("Bonjour les Simploniens")
+}
+
+
+
+
+
+
+
+function h1(Text){
+    var Nom = document.createElement("h1");
+    document.appendChild(h1);
+    Nom_Text = document.createTextNode("Bonjour les Simploniens")
+}
+
+
+
+
+
+const Nom = document.createElement("div")
+const Nom_Text = document.createTextNode("Bonjour les Simploniens")
+
+Nom.appendChild(Nom_Text);
